32521b79e2a396f22fe2f256457407e368dee3bc
ehababda <ehab.dewan@gmail.com>
2025-08-25 12:10:18 +0300

    Add project files.

---
+++ hw2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace hw2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ hw2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace hw2;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
